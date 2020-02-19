b3d58b4f3e85ea202b1affdb241f6fde82bc2e78
Hannah Tekie <hannahtekie@gmail.com>
2020-02-19 13:15:06 -0800

    Second wave completed

--- main.rb
+++ main.rb
@@ -1,10 +1,33 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
+solar_system = SolarSystem.new('Sol')
+
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
   puts earth.summary()
+  solar_system.add_planet(earth)
 
-  #why we do puts not return is b/c it's you want to save it and not print it. 
+  mars = Planet.new('Mars', 'reddish', 6.39*10e23, 1.4307e6, 'Named after the Roman god of war')
+  solar_system.add_planet(mars)
+  list = solar_system.list_planets
+  puts list
+
+  found_planet = solar_system.find_planet_by_name('Earth')
+  puts found_planet
+  puts found_planet.summary
+
+  
+    
 end
 
 main
+
+
+
+
+
+
+
+
+
--- solar_system.rb
+++ solar_system.rb
@@ -1,3 +1,28 @@
 class SolarSystem
+  attr_reader :star_name, :planets
+  
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+    end
 
+    def add_planet(planet)
+      @planets << planet
+    end
+    def list_planets 
+      @planets 
+      planet_names = []
+      planets.each do|planet|
+      planet_names << planet.name
+    end
+    planet_names
+  end
+
+  def find_planet_by_name(planet_names)
+    @planets.each do |planet|
+      if planet.name.upcase == planet_names.upcase
+      return planet
+      end
+    end
+  end
 end
